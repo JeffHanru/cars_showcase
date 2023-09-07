fbc3fe5561423aa628825042da492e4d85f66ed5
Hanru Xu <xjhzwx@gmail.com>
2023-09-07 21:14:16 +1000

    initial commit

---
+++ components/index.ts
@@ -0,0 +1,21 @@
+import Hero from "./Hero"
+import CustomButton from "./CustomButton"
+import Navbar from "./Navbar"
+import Footer from "./Footer"
+import SearchBar from "./SearchBar"
+import Filter from "./CustomFilter"
+import SearchManufacturer from "./SearchManufacturer"
+import CarCard from "./CarCard"
+import CarDetails from "./CarDetails"
+
+export {
+  Hero,
+  CustomButton,
+  Navbar,
+  Footer,
+  SearchBar,
+  Filter,
+  SearchManufacturer,
+  CarCard,
+  CarDetails
+}
